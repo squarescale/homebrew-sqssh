508c0839fbeb14456c5b93b49cd51ccf5d818192
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-07 17:33:36 +0000

    Brew formula update for sqssh version v0.1.10

--- sqssh.rb
+++ sqssh.rb
@@ -1,9 +1,9 @@
 class Sqssh < Formula
   desc ""
   homepage ""
-  url "https://github.com/squarescale/sqssh/releases/download/v0.1.8/sqssh_0.1.8_Darwin_x86_64.tar.gz"
-  version "0.1.8"
-  sha256 "a819b2698ccfb489c90fa8bb9577d4ba7b428327962f1aa12d4338540c4fcd07"
+  url "https://github.com/squarescale/sqssh/releases/download/v0.1.10/sqssh_0.1.10_Darwin_x86_64.tar.gz"
+  version "0.1.10"
+  sha256 "7c919376176748c42dae7f7a93edecc0531cefd4a1d0f01640c6cbdcc8586903"
 
   def install
     bin.install "sqssh"
